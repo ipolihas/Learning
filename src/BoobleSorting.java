93613f78a86ec77eeed276c25b3c9655510f9429
ipolihas <ivpolihas@gmail.com>
2017-03-03 01:31:45 +0200

    adding new project

---
+++ src/BoobleSorting.java
@@ -0,0 +1,28 @@
+
+public class BoobleSorting {
+	public static void main(String[] args) {
+
+		int[] array = { 30, 2, 10, 4, 6 };
+		boolean sort = true;
+		int i, k;
+		// int j;
+		int a = 0;
+		//for (j=0;j<array.length-1;j++){
+		while (sort) {
+			sort = false;
+			for (k = 0; k < (array.length - 1); k++) {
+				if (array[k] > array[k + 1]) {
+					a = array[k+1];
+					array[k+1] = array[k];
+					array[k] = a;
+					sort = true;
+				}
+
+			}
+		}
+		for (i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+
+	}
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,27 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+
+		int i, j, k = 1, n=5;
+		int a[][] = new int[5][5];
+
+		for (i = 0; i < 5; i++) {
+			for (j = 0; j < 5; j++) {
+				a[i][j] = k;
+				k++;
+				if (i == j | i + j == n-1) {
+					if (k<10) {
+					System.out.print(" *" + " ");
+					} else {
+						System.out.print(" *" + " ");
+						}
+				} else if (k<10){
+					System.out.print(" " + a[i][j] + " ");
+				} else {
+					System.out.print(a[i][j] + " ");
+				}
+			}
+			System.out.println(" ");
+		}
+	}
+
+}
